d8f608483afc8f2ed12ca55163ba934f73ef69cf
unknown <andimuhmuflih21@gmail.com>
2024-05-24 02:35:00 +0800

    feat: menambahkan  tombol pada scene 2 dan menambahkan icon pada setial tombol serta mengubah warna tombol, fix: memperbaiki susunan layout dan beberapa bug pada import file (muflih)

--- app/src/main/java/id/rezka/tuprak9/App.java
+++ app/src/main/java/id/rezka/tuprak9/App.java
@@ -5,6 +5,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -37,31 +38,83 @@ public class App extends Application {
     public static void setJadwal(String jadwal) {
         App.jadwal = jadwal;
     }
-
+    
     public Scene createMainScene(Stage primaryStage) {
-        Button button1 = new Button("Search");
-        button1.setStyle("-fx-font-weight: bold;");
-        button1.setMaxWidth(470);
-        button1.setMinHeight(20);
+        Button searchbar = new Button("Search");
+        searchbar.setStyle(
+            "-fx-font-weight: bold;" +
+            "-fx-font-size: 10px;" +
+            "-fx-background-color: rgba(255, 255, 255, 255);");
+            searchbar.setMaxWidth(470);
+            searchbar.setMinHeight(30);
+            
+            StackPane stackPane1 = new StackPane(searchbar);
+            stackPane1.setAlignment(Pos.TOP_CENTER);
+            stackPane1.setPadding(new javafx.geometry.Insets(20));
+            
+            try {
+                FileInputStream iconStream = new FileInputStream("src/main/resources/image/search.png");
+                Image icon = new Image(iconStream);
+        
+                ImageView imageView = new ImageView(icon);
+                imageView.setFitHeight(10);
+                imageView.setFitWidth(10);
+                searchbar.setGraphic(imageView);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
 
-        StackPane stackPane1 = new StackPane(button1);
-        stackPane1.setAlignment(Pos.TOP_CENTER);
-        stackPane1.setPadding(new javafx.geometry.Insets(10));
-
-        Button button2 = new Button("To Do");
+        Button button2 = new Button("");
         button2.setMaxWidth(130);
         button2.setMinHeight(80);
-        button2.setStyle("-fx-font-size: 25px; -fx-font-weight: bold;");
+        button2.setStyle("-fx-font-size: 25px;"+
+        "-fx-font-weight: bold;" + 
+        "-fx-background-color: rgba(128, 128, 128, 255);" +
+        "-fx-background-radius: 20");
 
         StackPane stackPane2 = new StackPane(button2);
         stackPane2.setAlignment(Pos.CENTER_LEFT);
-        stackPane2.setPadding(new javafx.geometry.Insets(15));
+        stackPane2.setPadding(new javafx.geometry.Insets(0, 10, 10, 20));
 
-        Button jadwalButton = new Button("+ Add Schedule");
-        jadwalButton.setMaxHeight(150);
-        jadwalButton.setMinHeight(80);
+        try {
+            FileInputStream iconStream = new FileInputStream("src/main/resources/image/calendar.png");
+            Image icon = new Image(iconStream);
+
+            ImageView imageView = new ImageView(icon);
+            imageView.setFitHeight(70);
+            imageView.setFitWidth(70);
+            button2.setGraphic(imageView);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Button jadwalButton = new Button(" Add Schedule ");
+        jadwalButton.setMaxHeight(100);
+        jadwalButton.setMinHeight(10);
         jadwalButton.setAlignment(Pos.BASELINE_RIGHT);
-        jadwalButton.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
+        jadwalButton.setStyle(
+            "-fx-font-size: 13px;" +
+            "-fx-font-weight: bold;" + 
+            "-fx-background-color: rgba(128, 128, 128, 255);" + 
+            "-fx-text-fill: black;" + 
+            "-fx-font-family: 'Arial';");
+
+            StackPane stackPane3 = new StackPane(jadwalButton);
+            stackPane3.setAlignment(Pos.BOTTOM_LEFT);
+            stackPane3.setPadding(new javafx.geometry.Insets(350, 0, 0, 360));
+
+            try {
+                FileInputStream iconStream = new FileInputStream("src/main/resources/image/plus.png");
+                Image icon = new Image(iconStream);
+    
+                ImageView imageView = new ImageView(icon);
+                imageView.setFitHeight(10);
+                imageView.setFitWidth(10);
+                jadwalButton.setGraphic(imageView);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+       
 
        
         jadwalButton.setOnAction(e -> {
@@ -72,12 +125,9 @@ public class App extends Application {
             }
         });
 
-        StackPane stackPane3 = new StackPane(jadwalButton);
-        stackPane3.setAlignment(Pos.BOTTOM_LEFT);
-        stackPane3.setPadding(new javafx.geometry.Insets(40));
 
         VBox root = new VBox();
-        root.setStyle("-fx-background-color: #222831;");
+        root.setStyle("-fx-background-color: #0C0C0C;");
         root.getChildren().addAll(stackPane1, stackPane2, stackPane3); 
 
         Scene scene = new Scene(root, 500, 600);
--- app/src/main/java/id/rezka/tuprak9/InputJadwal.java
+++ app/src/main/java/id/rezka/tuprak9/InputJadwal.java
@@ -1,36 +1,67 @@
 package id.rezka.tuprak9;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class InputJadwal {
     public static Scene createScene(Stage primaryStage, App app) {
-        VBox layout = new VBox(15);
-        layout.setPadding(new javafx.geometry.Insets(20));
-        layout.setAlignment(Pos.BASELINE_RIGHT);
-        layout.setStyle("-fx-background-color: black; -fx-font-weight: bold; -fx-font-size: 25px");
-
+        
         Label catatanLabel = new Label("Catatan");
         catatanLabel.setStyle("-fx-text-fill: white;");
-
+        
         TextField jadwalField = new TextField();
         jadwalField.setPromptText("Masukkan Catatan");
         jadwalField.setStyle("-fx-background-color: white; -fx-text-fill: black; -fx-font-size: 17px;");
-
+        
         Button saveButton = new Button("Save");
         saveButton.setStyle("-fx-font-size: 17px;");
         saveButton.setOnAction(e -> {
             App.setJadwal(jadwalField.getText());
             primaryStage.setScene(app.createMainScene(primaryStage)); 
         });
+        
+        Button tmbhtugas = new Button("");
+        tmbhtugas.setMaxHeight(90);
+        tmbhtugas.setMaxWidth(10);
+        tmbhtugas.setAlignment(Pos.BASELINE_RIGHT);
+        tmbhtugas.setStyle( "-fx-background-color: rgba(255, 255, 255, 255);" + 
+        "-fx-background-radius : 50%");
+        
+        StackPane tmbktgspane = new StackPane(tmbhtugas);
+        tmbktgspane.setAlignment(Pos.BOTTOM_CENTER);
+        tmbktgspane.setPadding(new javafx.geometry.Insets(300, 0, 0, 0));
 
-        layout.getChildren().addAll(catatanLabel, jadwalField, saveButton);
-
-        return new Scene(layout, 640, 480);
+        
+            try {
+                FileInputStream iconStream = new FileInputStream("src/main/resources/image/plus2.png");
+                Image icon = new Image(iconStream);
+    
+                ImageView imageView = new ImageView(icon);
+                imageView.setFitHeight(20);
+                imageView.setFitWidth(20);
+                tmbhtugas.setGraphic(imageView);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+       
+        
+        VBox layout = new VBox(15);
+        layout.setPadding(new javafx.geometry.Insets(20));
+        layout.setAlignment(Pos.BASELINE_RIGHT);
+        layout.setStyle("-fx-background-color: black; -fx-font-weight: bold; -fx-font-size: 25px");
+        layout.getChildren().addAll(catatanLabel, jadwalField, saveButton, tmbktgspane);
+        
+        return new Scene(layout, 500, 600);
     }
 }
