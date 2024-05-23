30815ca27d219364a719a69522aa5c5ed403a127
unknown <rreska9@gmail.com>
2024-05-23 17:43:56 +0800

    feat: menambahkan icon aplikasi (Rezka)

--- app/src/main/java/id/rezka/tuprak9/App.java
+++ app/src/main/java/id/rezka/tuprak9/App.java
@@ -4,32 +4,45 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
-        // primaryStage.setTitle("Reminder's");
-        // primaryStage.setResizable(false);
+        primaryStage.setTitle("Reminder's");
+        primaryStage.setResizable(false);
 
-        // Button button1 = new Button("..");
-        // button1.setMaxWidth(470);
-        // button1.setMaxHeight(40);
+        Button button1 = new Button("Search");
+        button1.setMaxWidth(470);
+        button1.setMaxHeight(45);
     
 
-        // StackPane stackPane = new StackPane(button1);
-        // stackPane.setAlignment(Pos.TOP_CENTER);
-        // stackPane.setPadding(new javafx.geometry.Insets(10));
+        StackPane stackPane = new StackPane(button1);
+        stackPane.setAlignment(Pos.TOP_CENTER);
+        stackPane.setPadding(new javafx.geometry.Insets(10));
 
-        // VBox root = new VBox(stackPane);
-        // root.setStyle("-fx-background-color: #222831;");
+        VBox root = new VBox(stackPane);
+        root.setStyle("-fx-background-color: #222831;");
 
-        // Scene scene = new Scene(root, 500, 600);
+        Scene scene = new Scene(root, 500, 600);
 
-        // primaryStage.setScene(scene);
-        // primaryStage.show();
+        try {
+            FileInputStream iconStream = new FileInputStream("src/main/resources/image/iconApp.png");
+            Image icon = new Image(iconStream);
+            primaryStage.getIcons().add(icon);
+            } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
--- app/src/main/java/scene/home.java
+++ app/src/main/java/scene/home.java
@@ -1,39 +1,39 @@
-package scene;
+// package scene;
 
-import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-public class home  extends Application{
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-         primaryStage.setResizable(false);
+// import javafx.application.Application;
+// import javafx.geometry.Pos;
+// import javafx.scene.Scene;
+// import javafx.scene.control.Button;
+// import javafx.scene.layout.StackPane;
+// import javafx.scene.layout.VBox;
+// import javafx.stage.Stage;
+// public class home  extends Application{
+//     @Override
+//     public void start(Stage primaryStage) throws Exception {
+//          primaryStage.setResizable(false);
 
-        Button button1 = new Button("..");
-        button1.setMaxWidth(470);
-        button1.setMaxHeight(40);
+//         Button button1 = new Button("..");
+//         button1.setMaxWidth(470);
+//         button1.setMaxHeight(40);
     
 
-        StackPane stackPane = new StackPane(button1);
-        stackPane.setAlignment(Pos.TOP_CENTER);
-        stackPane.setPadding(new javafx.geometry.Insets(10));
+//         StackPane stackPane = new StackPane(button1);
+//         stackPane.setAlignment(Pos.TOP_CENTER);
+//         stackPane.setPadding(new javafx.geometry.Insets(10));
 
-        VBox root = new VBox(stackPane);
-        root.setStyle("-fx-background-color: #222831;");
+//         VBox root = new VBox(stackPane);
+//         root.setStyle("-fx-background-color: #222831;");
 
-        Scene scene = new Scene(root, 500, 600);
+//         Scene scene = new Scene(root, 500, 600);
 
-        primaryStage.setScene(scene);
-        primaryStage.show();
+//         primaryStage.setScene(scene);
+//         primaryStage.show();
         
-    }
+//     }
     
 
-    public static void main(String[] args) {
-        launch(args);
+//     public static void main(String[] args) {
+//         launch(args);
         
-    }
-}
+//     }
+// }
