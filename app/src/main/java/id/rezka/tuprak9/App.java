14753f15490d70a9d9534ad0cf768fc9a487b6be
unknown <rreska9@gmail.com>
2024-05-23 18:23:25 +0800

    feat: menambahkan tombol To Do (Rezka)

--- app/src/main/java/id/rezka/tuprak9/App.java
+++ app/src/main/java/id/rezka/tuprak9/App.java
@@ -9,7 +9,6 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
@@ -21,15 +20,24 @@ public class App extends Application {
 
         Button button1 = new Button("Search");
         button1.setMaxWidth(470);
-        button1.setMaxHeight(45);
-    
+        button1.setMinHeight(20);
 
-        StackPane stackPane = new StackPane(button1);
-        stackPane.setAlignment(Pos.TOP_CENTER);
-        stackPane.setPadding(new javafx.geometry.Insets(10));
+        StackPane stackPane1 = new StackPane(button1);
+        stackPane1.setAlignment(Pos.TOP_CENTER);
+        stackPane1.setPadding(new javafx.geometry.Insets(10));
 
-        VBox root = new VBox(stackPane);
+        Button button2 = new Button("To Do");
+        button2.setMaxWidth(150);
+        button2.setMinHeight(80);
+        button2.setStyle("-fx-font-size: 25px;");
+
+        StackPane stackPane2 = new StackPane(button2);
+        stackPane2.setAlignment(Pos.CENTER_LEFT);
+        stackPane2.setPadding(new javafx.geometry.Insets(15));
+
+        VBox root = new VBox();
         root.setStyle("-fx-background-color: #222831;");
+        root.getChildren().addAll(stackPane1, stackPane2);
 
         Scene scene = new Scene(root, 500, 600);
 
@@ -37,7 +45,7 @@ public class App extends Application {
             FileInputStream iconStream = new FileInputStream("src/main/resources/image/iconApp.png");
             Image icon = new Image(iconStream);
             primaryStage.getIcons().add(icon);
-            } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
