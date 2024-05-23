8778d70fb14fe41ce2ec0f4c3e6d1db98d7205c1
unknown <rreska9@gmail.com>
2024-05-23 16:06:38 +0800

    feat:mendeklarasikan stage & button search (rezka & muflih)

---
+++ app/src/main/java/id/rezka/tuprak9/App.java
@@ -0,0 +1,38 @@
+package id.rezka.tuprak9;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        // primaryStage.setTitle("Reminder's");
+        // primaryStage.setResizable(false);
+
+        // Button button1 = new Button("..");
+        // button1.setMaxWidth(470);
+        // button1.setMaxHeight(40);
+    
+
+        // StackPane stackPane = new StackPane(button1);
+        // stackPane.setAlignment(Pos.TOP_CENTER);
+        // stackPane.setPadding(new javafx.geometry.Insets(10));
+
+        // VBox root = new VBox(stackPane);
+        // root.setStyle("-fx-background-color: #222831;");
+
+        // Scene scene = new Scene(root, 500, 600);
+
+        // primaryStage.setScene(scene);
+        // primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
