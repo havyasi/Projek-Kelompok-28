b61d9aeda9182d95cfc4243ba84733e913ca2999
unknown <rreska9@gmail.com>
2024-05-26 09:06:02 +0800

    feat: Membuat tombol kembali ke stage awal di dalam search scene

--- app/src/main/java/id/rezka/tuprak9/SearchScene.java
+++ app/src/main/java/id/rezka/tuprak9/SearchScene.java
@@ -2,6 +2,7 @@ package id.rezka.tuprak9;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -9,18 +10,22 @@ import javafx.stage.Stage;
 public class SearchScene {
 
     public static Scene createScene(Stage primaryStage, App appInstance) {
+        Button backButton = new Button("Back");
+        backButton.setStyle("-fx-font-size: 13px; -fx-font-weight: bold;");
+        backButton.setOnAction(e -> primaryStage.setScene(appInstance.createMainScene(primaryStage)));
+
         TextField searchField = new TextField();
         searchField.setPromptText("Search");
         searchField.setStyle("-fx-background-color: white; -fx-text-fill: black; -fx-font-size: 15px;");
         searchField.setPrefWidth(400);
 
-        VBox layout2 = new VBox(10, searchField);
+        VBox layout2 = new VBox(10,backButton ,searchField);
         layout2.setPadding(new javafx.geometry.Insets(20));
-        layout2.setAlignment(Pos.BASELINE_LEFT);
+        layout2.setAlignment(Pos.TOP_LEFT);
         layout2.setStyle("-fx-background-color: #7296a4; -fx-font-weight: bold; -fx-font-size: 25px");
 
         return new Scene(layout2, 500, 600);
     }
-    }
+}
     
 
